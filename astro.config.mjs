91a09125c04def36ff1f48bdad212be563e8c737
Karthik Nayak <nayakkarthik466@gmail.com>
2025-04-13 12:55:54 +0530

    Add vercel package for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 import icon from "astro-icon";
-
 import db from "@astrojs/db";
-
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://karthiknayak.in",
   integrations: [tailwind(), mdx(), icon(), db(), sitemap()],
+  output: "server",
+  adapter: vercel(),
 });
